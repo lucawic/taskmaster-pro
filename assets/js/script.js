753c258932c6fc3626f96e81c7bb565052dea7b2
lucawic <lucaswick@email.arizona.edu>
2021-07-08 01:01:29 -0700

    added trash and styled task boxes w/grid/colors/etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,9 +62,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 // save button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
+ 
 
   if (taskText && taskDate) {
     createTask(taskText, taskDate, "toDo");
